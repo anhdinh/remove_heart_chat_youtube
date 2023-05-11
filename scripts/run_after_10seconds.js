6dc6a5b2018e9a136626c41e89519e941c5cd6ac
Andy <dta89uct@gmail.com>
2023-05-12 00:59:32 +0700

    remove nimo-iframe in chat box

--- scripts/run_after_10seconds.js
+++ scripts/run_after_10seconds.js
@@ -1,22 +1,34 @@
 
 jQuery(function(){
-  runIfdocumentIsReady();
+  runIfdocumentIsReady(); 
 });
-
 
 function runIfdocumentIsReady(){ 
   setInterval(function(){
-    const chatframe = document.getElementById('chatframe');
-    if (chatframe) {
-      removeHeartFromChatFrame(chatframe);
-    }
+    removeBannerChatNimo();
+    removeHeartFromYoutube()
   },3000)
 }
 
+function removeHeartFromYoutube(){
+  const chatframe = document.getElementById('chatframe');
+  if (chatframe) {
+    removeHeartFromChatFrame(chatframe);
+  }
+}
 
 function removeHeartFromChatFrame(chatframe){
   var reaction_control_panel  = chatframe.contentWindow.document.getElementById('reaction-control-panel');
   if(reaction_control_panel) {
     reaction_control_panel.remove()  
+  }
+}
+
+function removeBannerChatNimo(){
+  if(window.location.host==='www.nimo.tv'){
+    let iframe_pannel = document.getElementsByClassName('nimo-iframe__wrap movePannel react-draggable')[0];
+    if(iframe_pannel){
+      iframe_pannel.remove()
+    }
   }
 }
